070457325e10519127e534122853a05da7f4dd2e
nrupeshbabu <nrupesh08@gmail.com>
2020-01-10 11:14:11 +0530

    Adding Style(class) in forms

--- newsblog/users/forms.py
+++ newsblog/users/forms.py
@@ -4,7 +4,10 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField()
+    username = forms.CharField(max_length=255,widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
